65f3d7d85d2ebffaea72706aae60fce433f35663
Jason Yim <jyim3@hawaii.edu>
2018-10-27 21:22:01 -1000

    created item resource

--- app.py
+++ app.py
@@ -1,58 +1,25 @@
 from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-stores = [
-	{
-		'name': 'My Store',
-		'items': [
-			{
-			'name': 'My Item',
-			'price': 15.99
-			}
-		]
-	}
-]
+items = []
 
-@app.route('/store', methods=['POST'])
-def create_store():
-	request_data = request.get_json()
-	new_store = {
-		'name': request_data['name'],
-		'items': []
-	}
-	stores.append(new_store)
-	return jsonify(new_store)
+class Item(Resource):
+	def get(self, name):
+		for item in items:
+			if item['name'] == name:
+				return item
+		return {'item': None}, 404
 
-@app.route('/store/<string:name>')
-def get_store(name):
-	for store in stores:
-		if store['name'] == name:
-			return jsonify(store)
-	return jsonify({'message': 'store not found'})
 
-@app.route('/store')
-def get_stores():
-	return jsonify({'stores': stores})
+	def post(self, name):
+		data = request.get_json()
+		item = {'name': name, 'price': data['price']}
+		items.append(item)
+		return item, 201
 
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-	request_data = request.get_json()
-	for store in stores:
-		if store['name'] == name:
-			new_item = {
-				'name': request_data['name'],
-				'price': request_data['price']
-			}
-			store['items'].append(new_item)
-			return jsonify(new_item)
-	return jsonify({'message': 'store not found'})
+api.add_resource(Item, '/item/<string:name>')
 
-@app.route('/store/<string:name>/item')
-def get_items_in_store(name):
-	for store in stores:
-		if store['name'] == name:
-			return jsonify({'items': store['items']})
-	return jsonify({'message': 'store not found'})
-	
 app.run(port=5000)
