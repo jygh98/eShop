0a903729a19e7b0d350828e1b454105165c39b33
Jason Yim <jyim3@hawaii.edu>
2018-10-27 21:26:53 -1000

    added request parsing to filter by valid inputs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparser
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,15 +7,21 @@ api = Api(app)
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
 	def get(self, name):
 		item = next(filter(lambda x: x['name'] == name, items), None)
 		return {'item': item}, 200 if item else 404
 
 
 	def post(self, name):
+		data = Item.parser.parse_args()
 		if next(filter(lambda x: x['name'] == name, items), None):
 			return {'message': "An item with name '{}' already exists".format(name)}, 400
-		data = request.get_json()
 		item = {'name': name, 'price': data['price']}
 		items.append(item)
 		return item, 201
@@ -27,7 +33,7 @@ class Item(Resource):
 
 
 	def put(self, name):
-		data = request.get_json()
+		data = Item.parser.parse_args()
 		item = next(filter(lambda x: x['name'] == name, items), None)
 		if item is None:
 			item = {'name': name, 'price': data['price']}
