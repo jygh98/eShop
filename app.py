4d054b8a09b9e33877bb2583d2ac7076ef72bea6
Jason Yim <jyim3@hawaii.edu>
2018-10-27 21:22:28 -1000

    created itemlist resource

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ class Item(Resource):
 		items.append(item)
 		return item, 201
 
+class ItemList(Resource):
+	def get(self):
+		return {'items': items}
+
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
 app.run(port=5000)
