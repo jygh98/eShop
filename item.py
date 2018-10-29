5ea13bac1e3f323c633cccf8ad24f591ca79ce9e
Jason Yim <jyim3@hawaii.edu>
2018-10-28 23:48:17 -1000

    updated PUT method

--- item.py
+++ item.py
@@ -63,13 +63,34 @@ class Item(Resource):
 
 	def put(self, name):
 		data = Item.parser.parse_args()
-		item = next(filter(lambda x: x['name'] == name, items), None)
-		if item is None:
-			item = {'name': name, 'price': data['price']}
-			items.append(item)
-		else:
-			item.update(data)
-		return item
+
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
+
+        if item is None:
+            try:
+                self.insert(updated_item)
+            except:
+                return {'message': 'An error occurred inserting the item'}, 500
+        else:
+            try:
+                self.update(updated_item)
+            except:
+                return {'message': 'An error occurred updating the item'}, 500
+        return updated_item
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'Item deleted'}
 
 class ItemList(Resource):
 	def get(self):
