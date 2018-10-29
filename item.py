2f1856e0290d9d01b5bb31757d0f49874db3d657
Jason Yim <jyim3@hawaii.edu>
2018-10-28 23:33:17 -1000

    refactor app.py to handle only app delpoyment

---
+++ item.py
@@ -0,0 +1,44 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+class Item(Resource):
+    @jwt_required
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+	def get(self, name):
+		item = next(filter(lambda x: x['name'] == name, items), None)
+		return {'item': item}, 200 if item else 404
+
+
+	def post(self, name):
+		data = Item.parser.parse_args()
+		if next(filter(lambda x: x['name'] == name, items), None):
+			return {'message': "An item with name '{}' already exists".format(name)}, 400
+		item = {'name': name, 'price': data['price']}
+		items.append(item)
+		return item, 201
+
+	def delete(self, name):
+		global items
+		items = list(filter(lambda x: x['name'] != name, items))
+		return {'message': 'Item deleted'}
+
+
+	def put(self, name):
+		data = Item.parser.parse_args()
+		item = next(filter(lambda x: x['name'] == name, items), None)
+		if item is None:
+			item = {'name': name, 'price': data['price']}
+			items.append(item)
+		else:
+			item.update(data)
+		return item
+
+class ItemList(Resource):
+	def get(self):
+		return {'items': items}
