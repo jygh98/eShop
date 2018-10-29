f3584e771a99205fe147a6b55e4e002f85d2c799
Jason Yim <jyim3@hawaii.edu>
2018-10-28 23:46:39 -1000

    updated POST method

--- item.py
+++ item.py
@@ -10,8 +10,16 @@ class Item(Resource):
         required=True,
         help="This field cannot be left blank!"
     )
-	def get(self, name):
-		connection = sqlite3.connect('data.db')
+
+    def get(self, name):
+		item = Item.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
         query = "SELECT * FROM items WHERE name=?"
@@ -20,16 +28,32 @@ class Item(Resource):
         connection.close()
 
         if row:
-            return {'items': {'name': row[0], 'price': row[1]}}
+            return {'item': {'name': row[0], 'price': row[1]}}
         return {'message': 'Item not found'}, 404
 
 	def post(self, name):
-		data = Item.parser.parse_args()
-		if next(filter(lambda x: x['name'] == name, items), None):
-			return {'message': "An item with name '{}' already exists".format(name)}, 400
-		item = {'name': name, 'price': data['price']}
-		items.append(item)
-		return item, 201
+		if Item.find_by_name(name):
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+        data = Item.parser.parse_args()
+        item = {'name': name, 'price': data['price']}
+
+        try:
+            Item.insert(item)
+        except:
+            return {'message': 'An error occurred inserting the item.'}, 500
+
+        return item, 201
+
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
 
 	def delete(self, name):
 		global items
