3ebd3de293db2566eaae59141be4246dabbedced
Jason Yim <jyim3@hawaii.edu>
2018-11-03 11:36:24 -1000

    fixed find_by_id wrong argument given

--- code/security.py
+++ code/security.py
@@ -8,4 +8,4 @@ def authenticate(username, password):
 
 def identity(payload):
     user_id = payload['identity']
-    return UserModel.find_by_id(username)
+    return UserModel.find_by_id(user_id)
