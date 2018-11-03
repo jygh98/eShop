dab8654e8734fd5ee6bade770d2adc6c5ad98b3c
Jason Yim <jyim3@hawaii.edu>
2018-11-03 12:41:11 -1000

    created StoreModel, implemented model w/ exisitng code

--- code/resources/item.py
+++ code/resources/item.py
@@ -9,6 +9,12 @@ class Item(Resource):
         required=True,
         help="This field cannot be left blank!"
     )
+    parser = reqparse.RequestParser()
+    parser.add_argument('store_id',
+        type=int,
+        required=True,
+        help="Every item needs a store id."
+    )
 
     @jwt_required()
     def get(self, name):
@@ -22,7 +28,7 @@ class Item(Resource):
             return {'message': "An item with name '{}' already exists".format(name)}, 400
         data = Item.parser.parse_args()
         print(data)
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
             item.save_to_db()
@@ -42,7 +48,7 @@ class Item(Resource):
         data = Item.parser.parse_args()
         item = ItemModel.find_by_name(name)
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
         item.save_to_db()
